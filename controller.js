c9ebad21804faa533f2fb70c3ca2bd7679903322
boltgolt <boltgolt@gmail.com>
2017-07-23 16:51:23 +0200

    Code cleanup

--- controller.js
+++ controller.js
@@ -1,23 +1,29 @@
+// Import required modules
 const keypress = require("keypress")
 const colors = require("colors/safe")
 
 module.exports = {
+	// Contains state variable for throttle and both the horizontal and vertical axis
 	state: {
-		thr: 0,
-		hor: 0,
-		ver: 0
+		thr: 0, // Throttle
+		hor: 0, // Horizontal axis
+		ver: 0 // Vertical axis
 	},
-	start: function() {
 
-
-		// make `process.stdin` begin emitting "keypress" events
+	/**
+	 * Start in-terminal drone control interface
+	 * @param  {Fcuntion} endCallback Function to be called when the user quits the script
+	 */
+	start: function(endCallback) {
+		// Init keypress module
 		keypress(process.stdin);
 
-		process.stderr.write('\x1B[?25l')
+		// Hide terminal cursor
+		process.stderr.write("\x1B[?25l")
 
-
-		// listen for the "keypress" event
+		// Listen for the "keypress" event
 		process.stdin.on("keypress", function (ch, key) {
+			// Make a shorthand for the state object
 			let state = module.exports.state
 
 			// Some keys will not be passed, catch that
@@ -25,7 +31,9 @@ module.exports = {
 				return
 			}
 
+			// Match pressed key with actions
 			switch (key.name) {
+				// User pressed right, increase horizontal axis power until we hit the maxmimum
 				case "right":
 					if (Math.round(state.hor * 100) >= 100) {
 						state.hor = 1
@@ -33,7 +41,8 @@ module.exports = {
 					else {
 						state.hor += 0.05
 					}
-					break;
+					break
+				// User pressed left, decrease horizontal axis power until we hit the minimum
 				case "left":
 					if (Math.round(state.hor * 100) <= -100) {
 						state.hor = -1
@@ -41,23 +50,26 @@ module.exports = {
 					else {
 						state.hor -= 0.05
 					}
-					break;
+					break
+				// User pressed up, increase vertical axis power until we hit the maxmimum
 				case "up":
-				if (Math.round(state.ver * 100) >= 100) {
+					if (Math.round(state.ver * 100) >= 100) {
 						state.ver = 1
 					}
 					else {
 						state.ver += 0.05
 					}
-					break;
+					break
+				// User pressed down, decrease vertical axis power until we hit the minimum
 				case "down":
-				if (Math.round(state.ver * 100) <= -100) {
+					if (Math.round(state.ver * 100) <= -100) {
 						state.ver = -1
 					}
 					else {
 						state.ver -= 0.05
 					}
-					break;
+					break
+				// User pressed home (throttle up), increase throttle power until we hit the maxmimum
 				case "home":
 					if (Math.round(state.thr * 100) >= 200) {
 						state.thr = 2
@@ -65,7 +77,8 @@ module.exports = {
 					else {
 						state.thr += 0.05
 					}
-					break;
+					break
+				// User pressed home (throttle down), decrease throttle power until we hit the minimum
 				case "end":
 					if (Math.round(state.thr * 100) <= 0) {
 						state.thr = 0
@@ -73,7 +86,8 @@ module.exports = {
 					else {
 						state.thr -= 0.05
 					}
-					break;
+					break
+				// User pressed clear or num5 (reset), decrease throttle to landing power and reset the horizontal and vertical axis
 				case "clear":
 					state.thr = .35
 					state.hor = 0
@@ -81,83 +95,94 @@ module.exports = {
 					break;
 			}
 
+			// Print new state to the console
 			printState()
 
-			if (key && key.ctrl && key.name == 'c') {
+			// Catch a ctrl+C (SIGINT) or an escape or q keypress to quit the script
+			if (key && ((key.ctrl && key.name == "c") || key.name == "q" || key.name == "escape")) {
+				// Print a special state
 				printState("END")
 
+				// Let the index script clean up
+				endCallback()
+
+				// Give the terminal its cursor back and add some newlines
 				process.stderr.write('\x1B[?25h')
 				process.stdout.write("\n\n")
-				process.exit()
 			}
 		})
 
+		// Make STDIN ready to receive keypresses
 		process.stdin.setRawMode(true)
 		process.stdin.resume()
 
+		// Print the first state
 		printState("FIRST")
 	}
 }
 
+/**
+ * Print a nicely formatted view of the drone status
+ * @param  {String} special Set to specific values for specal events
+ */
 function printState(special) {
+	// If this is the first time we're printing the state, add a new line
 	if (special == "FIRST") {
 		process.stdout.write("\n")
 	}
+	// Otherwise, return to the start of the old line to overwrite it
 	else {
 		process.stdout.write("\r")
 	}
 
+	// Round the throttle to a human readable format
 	let thr = Math.round(module.exports.state.thr * 100) / 100
+	// Set the text as black and round it to 2 decimals
 	thr = colors.black(`  ${thr.toFixed(2)}  `)
 
-	if (module.exports.state.thr > 1.2) {
-		thr = colors.bgRed(thr)
-	}
-	else if (module.exports.state.thr > .7) {
-		thr = colors.bgYellow(thr)
-	}
-	else {
-		thr = colors.bgWhite(thr)
-	}
+	// Color the background red if the throttle is dangerously high
+	if (module.exports.state.thr > 1.2) thr = colors.bgRed(thr)
+	// Color the background yellow if the throttle is higher than normal
+	else if (module.exports.state.thr > .7) thr = colors.bgYellow(thr)
+	// Give it a normal white background for normal values
+	else thr = colors.bgWhite(thr)
 
+	// Round the horizontal axis to a human readable format
 	let hor = Math.round(module.exports.state.hor * 100) / 100
+	// Set the text as black, round it to 2 decimals and add extra space in front if it's a positive number (makes natative and positive numbers have the same position)
 	hor = colors.black(` ${(hor < 0 ? "" : " ")}${hor.toFixed(2)}  `)
 
-	if (module.exports.state.hor > .5 || module.exports.state.hor < -.5) {
-		hor = colors.bgRed(hor)
-	}
-	else if (module.exports.state.hor > .25 || module.exports.state.hor < -.25) {
-		hor = colors.bgYellow(hor)
-	}
-	else {
-		hor = colors.bgWhite(hor)
-	}
+	// Color the background red if the horizontal axis is dangerously high or low
+	if (module.exports.state.hor > .5 || module.exports.state.hor < -.5) hor = colors.bgRed(hor)
+	// Color the background yellow if the horizontal axis is higher or lower than normal
+	else if (module.exports.state.hor > .25 || module.exports.state.hor < -.25) hor = colors.bgYellow(hor)
+	// Give it a normal white background for normal values
+	else hor = colors.bgWhite(hor)
 
+	// Round the vertical axis to a human readable format
 	let ver = Math.round(module.exports.state.ver * 100) / 100
+	// Set the text as black, round it to 2 decimals and add extra space in front if it's a positive number (see above)
 	ver = colors.black(` ${(ver < 0 ? "" : " ")}${ver.toFixed(2)}  `)
 
-	if (module.exports.state.ver > .5 || module.exports.state.ver < -.5) {
-		ver = colors.bgRed(ver)
-	}
-	else if (module.exports.state.ver > .25 || module.exports.state.ver < -.25) {
-		ver = colors.bgYellow(ver)
-	}
-	else {
-		ver = colors.bgWhite(ver)
-	}
+	// Color the background red if the vertical axis is dangerously high or low
+	if (module.exports.state.ver > .5 || module.exports.state.ver < -.5) ver = colors.bgRed(ver)
+	// Color the background yellow if the vertical axis is higher or lower than normal
+	else if (module.exports.state.ver > .25 || module.exports.state.ver < -.25) ver = colors.bgYellow(ver)
+	// Give it a normal white background for normal values
+	else ver = colors.bgWhite(ver)
 
+	// Default the status line to In flight
 	let status = colors.bold.red("In flight ")
+	// If the throttle is very low the engines will turn off and it will be in idle
+	if (module.exports.state.thr < .25) status = colors.bold.green("Idle      ")
+	// If the throttle is low but not too low, the engine power will not be enough to lift off
+	else if (module.exports.state.thr < .4) status = colors.bold.yellow("Armed     ")
 
-	if (module.exports.state.thr < .2) {
-		status = colors.bold.green("Idle      ")
-	}
-	else if (module.exports.state.thr < .4) {
-		status = colors.bold.yellow("Armed     ")
-	}
-
+	// If this is the final print show the status as Stopped
 	if (special == "END") {
 		status = colors.bold.red("Stopped    ")
 	}
 
-	process.stdout.write(` T: ${thr}   H: ${hor}   V: ${ver}   ${status}`);
+	// Print everything to the console
+	process.stdout.write(` T: ${thr}   H: ${hor}   V: ${ver}   ${status}`)
 }
--- index.js
+++ index.js
@@ -1,32 +1,39 @@
+// Import required modules
 const wifi = require("node-wifi")
 const colors = require("colors/safe")
 const dgram = require("dgram")
 const net = require("net")
 
+// Import other node files
 const magic = require("./magic.js")
 const controller = require("./controller.js")
 
-
-
-
+// Hardcode drone IP and ports
 const drone = {
 	ip: "172.16.10.1",
 	tcpPort: 8888,
 	udpPort: 8895
 }
 
-let udpControl = dgram.createSocket("udp4")
+// Init TCP socket object, this socket is olny keeping the drone awake
 let tcpControl = new net.Socket()
-let tcpVideo = new net.Socket()
+// Init TCP socket object, this sockets is sending instructions to the drone
+let udpControl = dgram.createSocket("udp4")
 
+// Will shut down drone when true
 let shutDown = false
 
 // Init OS wifi integration
 wifi.init({
-	// Any wifi iface is fine
+	// Allow any wifi interface
 	iface: null
 })
 
+/**
+ * Prints a nice massage to the console
+ * @param  {String} text  Message to be shown
+ * @param  {Bool}   error When true, will print as error and quit the program
+ */
 global.print = function(text, error) {
 	// Get the current time and add a leading 0 when needed
 	let d = new Date()
@@ -34,6 +41,7 @@ global.print = function(text, error) {
 	let m = (d.getMinutes() < 10 ? "0" : "") + d.getMinutes()
 	let s = (d.getSeconds() < 10 ? "0" : "") + d.getSeconds()
 
+	// Turn the text red if this is a critical error
 	if (error) {
 		text = colors.red.bold(text)
 	}
@@ -41,116 +49,118 @@ global.print = function(text, error) {
 	// Write it all to console
 	process.stdout.write(`[${h}:${m}:${s}] ${text}\n`)
 
+	// If an error, stop the script
 	if (error) {
 		process.exit(1)
 	}
 }
 
+// Let the user know we're starting the scan
+print("Scanning for drone wifi network...")
 
-
-print("Scanning for drone wifi network");
-
-// Scan for wifi networks
+// Scan for availible wifi networks
 wifi.scan(function(err, networks) {
+	// Catch error and quit script
 	if (err) {
-		console.log(err)
 		print("Could not scan for wifi networks", true)
-	} else {
+	}
+	else {
+		// Default to false to catch no-drone error
 		let network = false
 
+		// Loop through wifi networks
 		for (let i in networks) {
+			// If network SSID looks like a done network, save that and stop the loop
 			if (/RC Leading-[a-f0-9]{6}/.test(networks[i].ssid)) {
 				network = networks[i]
 				break
 			}
 		}
 
+		// If we couldn't find a network, stop script with error
 		if (network === false) {
 			print("Could not find drone network", true)
 		}
 		else {
-			print(`Connecting to ${network.ssid} (${network.mac})`);
+			// Let the user know to what drone we're connecting
+			print(`Connecting to "${network.ssid}" (${network.mac})...`)
 
+			// Connect to drone network without a password (it's an open network)
 			wifi.connect({
 				ssid: network.ssid,
 				password: ""
 			}, function(err) {
+				// Crash with error if connecting failed
 				if (err) {
-					console.log(err)
-					console.error("Could connect to drone network");
-					process.exit(1)
+					print("Could not connect to drone network", true)
 				}
 
+				// Let the user know we're opening the TCP socket to the dronw
+				print("Opening control socket to drone...")
+
+				// Connect to the TCP server on the drone
 				tcpControl.connect(drone.tcpPort, drone.ip, function() {
+					// Send the magic packet that will wake the drone up (lights will stop flashing at this point)
 					tcpControl.write(magic.tcpStart)
 
-					print(colors.green("Connected to drone"))
-					controller.start()
+					// Let the user know we have a connection now
+					print(colors.green("Connected to drone!"))
 
-					udpControl.send(magic.defaultUdp, 0, magic.defaultUdp.length, drone.udpPort, drone.ip);
+					// Start the controller terminal interface
+					controller.start(stopDrone)
 
-					setInterval(function () {
-						function genChecksum(data) {
-							return (data[1] ^ data[2] ^ data[3] ^ data[4] ^ data[5]) & 0xFF
-						}
+					// Send the first UDP packet with all default fields, which will put al conrols in the natural position
+					udpControl.send(magic.defaultUdp, 0, magic.defaultUdp.length, drone.udpPort, drone.ip)
 
+					// Send a control UDP packet every 50ms
+					setInterval(function() {
+						// If the user has send the quit signal
 						if (shutDown) {
+							// Get the natural UDP datagram
 							let stopUdp = magic.defaultUdp
+							// Set the throttle to 1/256, which will shut down the rotors from any state
 							stopUdp[3] = 0x01
 
-							udpControl.send(stopUdp, 0, stopUdp.length, drone.udpPort, drone.ip)
-							return
+							// Send the datagram aff to the drone and stop here
+							return udpControl.send(stopUdp, 0, stopUdp.length, drone.udpPort, drone.ip)
 						}
 
-
+						// Get the natural UDP datagram
 						let data = magic.defaultUdp
 
+						// Translate the -1 to 1 value of the horizontal axis to 0-256 and insert it into the natural datagram
 						data[1] = Math.round((controller.state.hor + 1) * 127)
+						// Translate the -1 to 1 value of the vertical axis to 0-256 and insert it into the natural datagram
 						data[2] = Math.round((controller.state.ver + 1) * 127)
+						// Translate the 0 to 2 value of the throttle to 0-256 and insert it into the natural datagram
 						data[3] = Math.round(controller.state.thr * 127)
-						data[6] = genChecksum(data);
 
-						// console.log(data);
+						// Generate the mew checksum for this datagram
+						data[6] = (data[1] ^ data[2] ^ data[3] ^ data[4] ^ data[5]) & 0xFF
 
-						udpControl.send(data, 0, magic.defaultUdp.length, drone.udpPort, drone.ip);
-					}, 50);
-				});
-
-			});
-
+						// Send the datagram off to the drone
+						udpControl.send(data, 0, magic.defaultUdp.length, drone.udpPort, drone.ip)
+					}, 50)
+				})
+			})
 		}
 	}
 })
 
-
-
-tcpControl.on('data', function(data) {
-	// console.log("data from drone:");
-	// console.log(data);
-});
-
-tcpControl.on('close', function() {
-	console.log('tcpControl connection closed by drone')
-})
-//
-// tcpVideo.connect(drone.tcpPort, drone.ip, function() {
-// 	console.log("conn");
-// 	tcpControl.write(magic.videoStart);
-// });
-//
-// tcpVideo.on('data', function(data) {
-// 	console.log(data);
-// });
-
-
-
-process.on("SIGINT", function() {
-    console.log()
-	print(colors.red.bold("Stopping drone and quiting"))
-
+/**
+ * Stops drone gracefully
+ */
+function stopDrone() {
+	// Set the global accordingly
 	shutDown = true
 
+	// Wait .2s for the shutdown to reach the drone and stop the script
 	setTimeout(function () {
-		process.exit();
+		process.exit()
 	}, 200)
+}
+
+// If the TCP socket gets broken, let the user know and stop the script
+tcpControl.on("close", function() {
+	print("Connection closed by drone", true)
 })
